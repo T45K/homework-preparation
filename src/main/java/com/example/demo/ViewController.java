3f911cd176872a0068ac9a65a56419f65f50fa72
T45K <tasktas9@gmail.com>
2019-08-21 07:14:41 +0000

    format

--- src/main/java/com/example/demo/ViewController.java
+++ src/main/java/com/example/demo/ViewController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 class ViewController {
     @GetMapping
-    public String view(){
+    public String view() {
         return "view";
     }
 }
